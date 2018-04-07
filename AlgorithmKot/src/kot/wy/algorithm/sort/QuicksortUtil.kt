ffaac5f5e0da4b395bff5a6931d1f0c5dfbc1eb0
rain.wang <wang_rain@outlook.com>
2018-04-07 15:54:41 +0800

    增加排序校验及缺陷修复

--- AlgorithmKot/src/kot/wy/algorithm/sort/QuicksortUtil.kt
+++ AlgorithmKot/src/kot/wy/algorithm/sort/QuicksortUtil.kt
@@ -8,21 +8,22 @@ class QuicksortUtil {
 
     companion object {
         fun sort(array: ArrayList<Long>) :ArrayList<Long> {
-            var index = 0;
+
+            if(array.size==0) return array
+            var index = 0
+
             for (i in array.indices) {
                 if (array[i] < array[index]) {
                     array.add(index, array[i]);
-                    array.removeAt(i + 1);
-                    index++;
+                    array.removeAt(i + 1)
+                    index++
                 }
             }
 
-            if(index ==0) return array;
-
             val result = ArrayList<Long>(array.size)
-            result.addAll(sort(subToArrayList(array.subList(0,index))))
+            if(index > 0) result.addAll(sort(listToArrayList(array.subList(0,index))))
             result.add(array[index])
-            result.addAll(sort(subToArrayList(array.subList(index+1,array.size))))
+            result.addAll(sort(listToArrayList(array.subList(index+1,array.size))))
             return result
         }
 
@@ -30,23 +31,21 @@ class QuicksortUtil {
     }
 }
 
-fun <T> arrayToArrayList (array: Array<T>) :ArrayList<T>{
-    var list = ArrayList<T>(array.size)
-    for(t in array) list.add(t)
-    return list
-}
-
-fun <T> subToArrayList (array: List<T>) :ArrayList<T>{
-    var list = ArrayList<T>()
-    for(t in array) list.add(t)
-    return list
+fun <T> listToArrayList (list: List<T>) :ArrayList<T>{
+    val result = ArrayList<T>()
+    for (l in list) result.add(l)
+    return result
 }
 
 
 fun main(args: Array<String>) {
 
-    val list = arrayToArrayList(Array(10,{SnowFlake.getId(Random().nextInt().toLong())}))
+    val list =listToArrayList(Array(100, { SnowFlake.getId(Random().nextInt().toLong()) }).asList())
+//    val list = arrayListOf(-3291496448, 9165924150039621633, 9165924165898358786, 9165924179372584963, -553899446268, 9165924699732996101, 9165924196813336582, -2667028473, -591313469432, -4378206199)
 
+    println("原list为" + (if (SortValidation.validate(list)) "有序的" else "无序的"))
     println(list)
-    println(QuicksortUtil.sort(list))
+    val sortedList = QuicksortUtil.sort(list)
+    println("排序后list为" + (if (SortValidation.validate(sortedList)) "有序的" else "无序的"))
+    println(sortedList)
 }
---
+++ AlgorithmKot/src/kot/wy/algorithm/sort/SortValidation.kt
@@ -0,0 +1,22 @@
+package kot.wy.algorithm.sort
+
+/**
+ * 名称：  SortValidation
+ * 作者:   rain.wang
+ * 日期:   2018/4/5
+ * 简介:
+ *
+ */
+class SortValidation {
+    companion object {
+        fun validate(list: ArrayList<Long>):Boolean{
+            for(i in list.indices){
+                if(i < list.size-1 && list[i] > list[i+1]){
+                    println(i)
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
